7f975daaef8e4662b6000fcb6b146582e04bc46f
jouni <jouni.hamalainen@kolumbus.fi>
2015-11-06 11:10:21 +0200

    201511061110 database siridb

--- siri/src/main/java/fi/javaee/siri/JpaConfiguration.java
+++ siri/src/main/java/fi/javaee/siri/JpaConfiguration.java
@@ -97,7 +97,7 @@ public class JpaConfiguration {
 	public DataSource getDataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/jee");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/siridb");
 		dataSource.setUsername("root");
 		dataSource.setPassword("root66");
 
