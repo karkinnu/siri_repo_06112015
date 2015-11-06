3d76af43e86bc42f2bbada3f925ba1c3ffb80dbf
thor <kajkinnu@gmail.com>
2015-11-06 10:52:24 +0200

    jep

--- siri/src/main/java/fi/javaee/siri/JpaConfiguration.java
+++ siri/src/main/java/fi/javaee/siri/JpaConfiguration.java
@@ -35,7 +35,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * http://www.baeldung.com/2011/12/13/the-persistence-layer-with-spring-3-1-and-jpa/
  */
 
-// test comment
+// test comment kkn
 
 @Configuration
 @EnableTransactionManagement
