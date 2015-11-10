17e729f3ea533c822151479eb5fcbca563a8f888
jouni <jouni.hamalainen@kolumbus.fi>
2015-11-10 19:17:35 +0200

    201511101920 ..siri.login..

---
+++ siri/src/main/java/fi/javaee/siri/login/service/MiscService.java
@@ -0,0 +1,16 @@
+package fi.javaee.siri.login.service;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+public interface MiscService {
+
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public abstract void palveluAdmineille();
+
+	@PreAuthorize("isAuthenticated()")
+	public abstract void palveluSisaankirjautuneille();
+
+	@PreAuthorize("permitAll")
+	public abstract void palveluKaikille();
+
+}
